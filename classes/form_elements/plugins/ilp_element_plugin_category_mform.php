f270f75ed3248d29810dd5e1a741e1c1da1b6c81
jonathan.pagel <jonathan.pagel@47a076d6-9262-0410-a75c-c7fd43826779>
2011-05-31 17:12:05 +0000

    tidying up

--- classes/form_elements/plugins/ilp_element_plugin_category_mform.php
+++ classes/form_elements/plugins/ilp_element_plugin_category_mform.php
@@ -4,4 +4,10 @@ require_once($CFG->dirroot.'/blocks/ilp/classes/form_elements/ilp_element_plugin
 
 class ilp_element_plugin_category_mform  extends ilp_element_plugin_mform_itemlist {
 
+	function __construct($report_id,$plugin_id,$course_id,$creator_id,$reportfield_id=null) {
+		parent::__construct($report_id,$plugin_id,$course_id,$creator_id,$reportfield_id=null);
+        //remember to define $this->tablename and $this->items_tablename in the child class
+        $this->tablename = 'block_ilp_plu_cat';
+        $this->items_tablename = 'block_ilp_plu_cat_items';
+	}
 }
