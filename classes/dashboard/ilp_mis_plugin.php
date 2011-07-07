aeabb45e8430681da3fe0b134e4def5f79b96ac5
Nigel.Daley <Nigel.Daley@47a076d6-9262-0410-a75c-c7fd43826779>
2011-07-07 15:05:31 +0000

    added ilp_mis_plugin abstract class

---
+++ classes/dashboard/ilp_mis_plugin.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * An abstract class that holds methods and attributes common to all mis plugin
+ * classes.
+ *
+ * @abstract
+ *
+ * @copyright &copy; 2011 University of London Computer Centre
+ * @author http://www.ulcc.ac.uk, http://moodle.ulcc.ac.uk
+ * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @package ILP
+ * @version 2.0
+ */
+
+
+
+//require the ilp_plugin.php class 
+require_once($CFG->dirroot.'/blocks/ilp/classes/dashboard/ilp_plugin.php');
+
+abstract class ilp_mis_plugin extends ilp_plugin {
+	
+	public 		$templatefile;
+	
+	/*
+	 * This var should hold the connection to the mis database
+	 */
+	public		$mis; 
+	
+	/**
+     * Constructor
+     */
+    function __construct() {
+    	global	$CFG;
+    	
+		//set the directory where plugin files of type ilp_dashboard_tab are stored  
+    	$this->plugin_class_directory	=	$CFG->dirroot."/blocks/ilp/classes/dashboard/mis";
+    	
+    	//set the table that the details of these plugins are stored in
+    	$this->plugintable	=	"block_ilp_mis_plugin";
+    	
+    	//call the parent constructor
+    	parent::__construct();
+    	
+    	//set the name of the template file should be a html file with the same name as the class
+    	$this->templatefile		=	$this->plugin_class_directory.'/'.$this->name.'.html';
+    }
+	
+	
+	
+	
+	
+}
