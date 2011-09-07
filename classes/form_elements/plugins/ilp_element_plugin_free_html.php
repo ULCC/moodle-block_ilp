5992ad403cba7440c73eb8eba85060a1d44402ec
jonathan.pagel <jonathan.pagel@47a076d6-9262-0410-a75c-c7fd43826779>
2011-09-07 14:13:40 +0000

    added load method and corrected entry_form method

--- classes/form_elements/plugins/ilp_element_plugin_free_html.php
+++ classes/form_elements/plugins/ilp_element_plugin_free_html.php
@@ -7,33 +7,42 @@ class ilp_element_plugin_free_html extends ilp_element_plugin {
 	public $data_entry_tablename;
 
     function __construct() {
-        //no specific tables necessary
-/*
     	$this->tablename = "block_ilp_plu_fre";
     	$this->data_entry_tablename = "block_ilp_plu_fre_ent";
-*/
     	parent::__construct();
     }
 
+    public function load($reportfield_id) {
+		$reportfield		=	$this->dbc->get_report_field_data($reportfield_id);
+		if (!empty($reportfield)) {
+			$this->reportfield_id	=	$reportfield_id;
+			$this->plugin_id	=	$reportfield->plugin_id;
+			$this->label			=	$reportfield->label;
+			$this->description		=	$reportfield->description;
+			$this->req				=	0;
+			$this->position			=	$reportfield->position;
+            $this->audit_type       =   $this->audit_type();
+			return true;	
+        }
+		return false;	
+    }
     /**
     * this function returns the mform elements taht will be added to a report form
-	* inherited elements are enough - there will never be user input
+	*
     */
     public	function entry_form( &$mform ) {
     	
-/*
     	$fieldname	=	"{$this->reportfield_id}_field";
         //no description necessary for this class imho (jfp)
 
     	//html field for element label
         $mform->addElement(
             'html',
-            $this->value
+            $this->description
         );
 
         if (!empty($this->req)) $mform->addRule($fieldname, null, 'required', null, 'client');
         $mform->setType($fieldname, PARAM_RAW);
-*/
     }
 
     public function audit_type() {
@@ -49,10 +58,8 @@ class ilp_element_plugin_free_html extends ilp_element_plugin {
 
 	/**
      * create tables for this plugin
-     * not necessary for this class - contents will just get stored in the entry description field
      */
     public function install() {
-/*
         global $CFG, $DB;
 
         // create the table to store report fields
@@ -86,7 +93,47 @@ class ilp_element_plugin_free_html extends ilp_element_plugin {
         if(!$this->dbman->table_exists($table)) {
             $this->dbman->create_table($table);
         }
-*/
+        
+        
+	    // create the new table to store responses to fields
+        $table = new $this->xmldb_table( $this->data_entry_tablename );
+        $set_attributes = method_exists($this->xmldb_key, 'set_attributes') ? 'set_attributes' : 'setAttributes';
+
+        $table_id = new $this->xmldb_field('id');
+        $table_id->$set_attributes(XMLDB_TYPE_INTEGER, 10, XMLDB_UNSIGNED, XMLDB_NOTNULL, XMLDB_SEQUENCE);
+        $table->addField($table_id);
+        
+        $table_title = new $this->xmldb_field('value');
+        $table_title->$set_attributes(XMLDB_TYPE_TEXT);
+        $table->addField($table_title);
+
+        $table_report = new $this->xmldb_field('entry_id');
+        $table_report->$set_attributes(XMLDB_TYPE_INTEGER, 10, XMLDB_UNSIGNED, XMLDB_NOTNULL);
+        $table->addField($table_report);
+        
+        $table_maxlength = new $this->xmldb_field('parent_id');
+        $table_maxlength->$set_attributes(XMLDB_TYPE_INTEGER, 10, XMLDB_UNSIGNED, XMLDB_NOTNULL);
+        $table->addField($table_maxlength);
+        
+        $table_timemodified = new $this->xmldb_field('timemodified');
+        $table_timemodified->$set_attributes(XMLDB_TYPE_INTEGER, 10, XMLDB_UNSIGNED, XMLDB_NOTNULL);
+        $table->addField($table_timemodified);
+
+        $table_timecreated = new $this->xmldb_field('timecreated');
+        $table_timecreated->$set_attributes(XMLDB_TYPE_INTEGER, 10, XMLDB_UNSIGNED, XMLDB_NOTNULL);
+        $table->addField($table_timecreated);
+
+        $table_key = new $this->xmldb_key('primary');
+        $table_key->$set_attributes(XMLDB_KEY_PRIMARY, array('id'));
+        $table->addKey($table_key);
+        
+       	$table_key = new $this->xmldb_key($this->tablename.'_foreign_key');
+        $table_key->$set_attributes(XMLDB_KEY_FOREIGN, array('parent_id'), $this->tablename ,'id');
+        $table->addKey($table_key);
+        
+        if(!$this->dbman->table_exists($table)) {
+            $this->dbman->create_table($table);
+        }
     }
 
     public function uninstall() {
