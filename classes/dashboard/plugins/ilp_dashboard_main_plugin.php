65e4a145d3622625cafef60045ce4e74e0cfb6e9
Nigel.Daley <Nigel.Daley@47a076d6-9262-0410-a75c-c7fd43826779>
2011-11-10 13:17:29 +0000

    added defaulttab var to tabitem as a mismatch between the selected tab and the tab being dispalyed was being caused

--- classes/dashboard/plugins/ilp_dashboard_main_plugin.php
+++ classes/dashboard/plugins/ilp_dashboard_main_plugin.php
@@ -62,7 +62,7 @@ class ilp_dashboard_main_plugin extends ilp_dashboard_plugin {
 			$selectedtab	=	$PARSER->optional_param('selectedtab',$defaulttab,PARAM_RAW);
 			
 			//get the actual tab item that was selected
-			$tabitem		=	$PARSER->optional_param('tabitem','1:1',PARAM_RAW);
+			$tabitem		=	$PARSER->optional_param('tabitem',$defaulttab.':1',PARAM_RAW);
 			
 			$tabs = array();
    			$tabrows = array();
