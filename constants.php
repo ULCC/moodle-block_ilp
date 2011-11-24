db4806aaf823448f6f7a30f141328e6c1f0d2827
Matt Gibson <matt@inaura.net>
2011-11-24 11:57:15 +0000

    Fixed error about constants being already defined

--- constants.php
+++ constants.php
@@ -1,26 +1,33 @@
-<?php 
+<?php
 
 
 // -----------------------------------------------------------------------------
 // --                         GENERAL STUFF                                   --
 // -----------------------------------------------------------------------------
 //param array defined with 0x40000 in line with other mooodle PARAM constant
-define('PARAM_ARRAY',               0x40000);
+if (!defined('PARAM_ARRAY')) {
+    define('PARAM_ARRAY',               0x40000);
+}
 
 // a blacklist of file extensions commonly blocked because they can carry viruses
-define('FILE_EXTENSION_BLACKLIST', 'ade, adp, app, asp, bas, bat, chm, cmd, com, cpl, crt, csh,exe, fxp, hlp, hta, htr, inf, ins, isp, jar, js, jse, ksh, lnk, mda, mdb, mde, mdt, mdw, mdz, mht, msc, msi, msp, mst, ops, pcd, pif, prf, prg, reg, scf, scr, sct, shb, shs, url, vb, vbe, vbs, wsc, wsf, wsh');
-
-define('REDIRECT_DELAY',            1);
-
-define('MAXLENGTH_BREADCRUMB',      130);
-
-define('BLOCK_NAME',                'ilp');
+if (!defined('FILE_EXTENSION_BLACKLIST')) {
+    define('FILE_EXTENSION_BLACKLIST', 'ade, adp, app, asp, bas, bat, chm, cmd, com, cpl, crt, csh,exe, fxp, hlp, hta, htr, inf, ins, isp, jar, js, jse, ksh, lnk, mda, mdb, mde, mdt, mdw, mdz, mht, msc, msi, msp, mst, ops, pcd, pif, prf, prg, reg, scf, scr, sct, shb, shs, url, vb, vbe, vbs, wsc, wsf, wsh');
+}
+if (!defined('REDIRECT_DELAY')) {
+    define('REDIRECT_DELAY',            1);
+}
+if (!defined('MAXLENGTH_BREADCRUMB')) {
+    define('MAXLENGTH_BREADCRUMB',      130);
+}
+if (!defined('BLOCK_NAME')) {
+    define('BLOCK_NAME',                'ilp');
+}
 
 //used when changing the position of a field in a report
 define('MOVE_UP',                  '1');
 define('MOVE_DOWN',                '0');
 
-//used by the date and date_deadline plugin to define what type of date may be 
+//used by the date and date_deadline plugin to define what type of date may be
 //accepted in a report
 define('PASTDATE',    1 );
 define('PRESENTDATE', 2 );
@@ -51,10 +58,18 @@ define( 'ILP_DEFAULT_USERSTATUS_RECORD' , '1' );
 define( 'ILP_DEFAULT_USER_ID' , '1' );
 
 //constants used by ilp_logging class
-define( 'LOG_ADD', 1 );
-define( 'LOG_UPDATE', 2 );
-define( 'LOG_DELETE', 3 );
-define( 'LOG_ASSESSMENT', 4 );
+if (!defined('LOG_ADD')) {
+    define( 'LOG_ADD', 1 );
+}
+if (!defined('LOG_UPDATE')) {
+    define( 'LOG_UPDATE', 2 );
+}
+if (!defined('LOG_DELETE')) {
+    define( 'LOG_DELETE', 3 );
+}
+if (!defined('LOG_ASSESSMENT')) {
+    define( 'LOG_ASSESSMENT', 4 );
+}
 
 //default css passfail colours
 define('ILP_CSSCOLOUR_FAIL','#FF0000');
