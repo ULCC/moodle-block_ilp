271d0a30aba706885afe2be8f0bd18fc323905ca
Nigel Daley <nigel.daley@gmail.com>
2011-12-08 18:48:43 +0000

    [FIXED #21148697] utilities directory has been added with clearmis.php file which enables mis settings to be easily cleared

--- utilities/clearmis.php
+++ utilities/clearmis.php
@@ -9,26 +9,33 @@
 
 require("../../../config.php");
 
-$dbconnection = mysql_pconnect($CFG->dbhost, $CFG->dbuser, $CFG->dbpass) or trigger_error(mysql_error(),E_USER_ERROR);
+require_login();
 
-mysql_select_db($CFG->dbname,$dbconnection);
+global $USER;
 
-$sql            = "SELECT * FROM {$CFG->prefix}config_plugins WHERE plugin = 'block_ilp' AND name LIKE '%db%'";
-$results        = mysql_query($sql, $dbconnection) or die(mysql_error());
-$record        = mysql_fetch_assoc($results);
-$queryres       =   array();
-do {
-    $updatesql  =   "UPDATE {$CFG->prefix}config_plugins SET value=' ' WHERE id = $record[id]";
-    $Result1 = mysql_query($updatesql, $dbconnection) or die(mysql_error());
+if (is_admin($USER))    {
+    $dbconnection = mysql_pconnect($CFG->dbhost, $CFG->dbuser, $CFG->dbpass) or trigger_error(mysql_error(),E_USER_ERROR);
 
-    $queryres[]  =   (!empty($Result1))  ? $record['name']." value has been cleared" : "<strong style='color:red;'>".$record['name']." has not been cleared </strong>";
+    mysql_select_db($CFG->dbname,$dbconnection);
 
-} while ($record = mysql_fetch_assoc($results));
+    $sql            = "SELECT * FROM {$CFG->prefix}config_plugins WHERE plugin = 'block_ilp' AND name LIKE '%db%'";
+    $results        = mysql_query($sql, $dbconnection) or die(mysql_error());
+    $record        = mysql_fetch_assoc($results);
+    $queryres       =   array();
+    do {
+        $updatesql  =   "UPDATE {$CFG->prefix}config_plugins SET value=' ' WHERE id = $record[id]";
+        $Result1 = mysql_query($updatesql, $dbconnection) or die(mysql_error());
 
-echo "<h1>Clear MIS</h1>";
+        $queryres[]  =   (!empty($Result1))  ? $record['name']." value has been cleared" : "<strong style='color:red;'>".$record['name']." has not been cleared </strong>";
 
-foreach ($queryres as $qr) {
-    echo $qr."<br />";
+    } while ($record = mysql_fetch_assoc($results));
+
+    echo "<h1>Clear MIS</h1>";
+
+    foreach ($queryres as $qr) {
+        echo $qr."<br />";
+    }
+} else {
+    echo 'only a moodle admin may use this file';
 }
-
 ?>
