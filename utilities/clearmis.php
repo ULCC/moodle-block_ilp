75c75c791329de183d23c8af712edb246cf4c9ec
Nigel Daley <nigel.daley@gmail.com>
2011-12-08 18:47:30 +0000

    [FIXED #21148697] utilities directory has been added with clearmis.php file which enables mis settings to be easily cleared

---
+++ utilities/clearmis.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Nigel.Daley
+ * Date: 08/12/11
+ * Time: 18:00
+ * This file clear the mis details from a mysql moodle database only. If another type of databse is in use then you will have to convert the file (not hard) yourself.
+ */
+
+require("../../../config.php");
+
+$dbconnection = mysql_pconnect($CFG->dbhost, $CFG->dbuser, $CFG->dbpass) or trigger_error(mysql_error(),E_USER_ERROR);
+
+mysql_select_db($CFG->dbname,$dbconnection);
+
+$sql            = "SELECT * FROM {$CFG->prefix}config_plugins WHERE plugin = 'block_ilp' AND name LIKE '%db%'";
+$results        = mysql_query($sql, $dbconnection) or die(mysql_error());
+$record        = mysql_fetch_assoc($results);
+$queryres       =   array();
+do {
+    $updatesql  =   "UPDATE {$CFG->prefix}config_plugins SET value=' ' WHERE id = $record[id]";
+    $Result1 = mysql_query($updatesql, $dbconnection) or die(mysql_error());
+
+    $queryres[]  =   (!empty($Result1))  ? $record['name']." value has been cleared" : "<strong style='color:red;'>".$record['name']." has not been cleared </strong>";
+
+} while ($record = mysql_fetch_assoc($results));
+
+echo "<h1>Clear MIS</h1>";
+
+foreach ($queryres as $qr) {
+    echo $qr."<br />";
+}
+
+?>
