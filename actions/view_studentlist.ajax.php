d9c02da26851badb9896bb710f8fd919f18259fa
Thomas Worthington <thomas.worthington@london.ac.uk>
2013-06-14 13:35:13 +0100

    [Fixed #51709595] Add MUC cache

--- actions/view_studentlist.ajax.php
+++ actions/view_studentlist.ajax.php
@@ -203,9 +203,14 @@ if(!empty($students))  {
 }
 
 $SESSION->ilp_prevnextstudents       =  serialize($prevnextstudents);
-
+$CACHE=cache::make('block_ilp','ilp_miscache');
 if (!empty($studentslist)) {
-   $all_record_counts=$dbc->count_all_report_entries(array_keys($studentslist));
+   $cachekey=implode(array_keys($studentslist),'|');
+   if(($all_record_counts=$CACHE->get($cachekey))===false)
+   {
+      $all_record_counts=$dbc->count_all_report_entries(array_keys($studentslist));
+      $CACHE->set($cachekey,$all_record_counts);
+   }
     foreach ($studentslist as $student) {
         $data = array();
 
---
+++ db/caches.php
@@ -0,0 +1,9 @@
+<?php
+
+$definitions = array(
+    'ilp_miscache' => array(
+       'mode' => cache_store::MODE_APPLICATION,
+       'persistent'=>true,
+       'ttl'=>600
+    )
+);
