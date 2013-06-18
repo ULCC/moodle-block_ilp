05a48b1d0923201fa631904dde062ba43115c24e
Thomas Worthington <thomas.worthington@london.ac.uk>
2013-06-18 14:47:49 +0100

    Mis debug option to dump results

--- classes/plugins/ilp_mis_plugin.class.php
+++ classes/plugins/ilp_mis_plugin.class.php
@@ -107,6 +107,8 @@ abstract class ilp_mis_plugin extends ilp_plugin
           ($r=$this->db->dbquery_sql($sql) or $r=array());
           $CACHE->set($sql,$r);
           $T->plap('Took ');
+          if($CFG->debug>1)
+             print_object($r);
        }
        else
        {
