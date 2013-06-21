7e515447b06dac439ed42ab2a3a0f8191cdaa5a4
Thomas Worthington <thomas.worthington@london.ac.uk>
2013-06-21 10:11:25 +0100

    Changed cache to 6hrs, so overnight changes are available in the morning

--- db/caches.php
+++ db/caches.php
@@ -1,9 +1,10 @@
 <?php
 
+//6 hr ttl
 $definitions = array(
     'ilp_miscache' => array(
        'mode' => cache_store::MODE_APPLICATION,
        'persistent'=>true,
-       'ttl'=>86400
+       'ttl'=>21600
     )
 );
