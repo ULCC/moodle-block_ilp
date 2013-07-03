965a8463b9297af5d7cbbfb5ecd91169dac96d1c
Thomas Worthington <thomas.worthington@london.ac.uk>
2013-07-03 10:08:43 +0100

    Whitespace tidy

--- block_ilp.php
+++ block_ilp.php
@@ -11,226 +11,222 @@
 
 class block_ilp extends block_list {
 
-    /**
-     * Sets initial block variables. Part of the blocks API
-     *
-     * @return void
-     */
-    function init() {
-        $this->title = get_string('blockname', 'block_ilp');
-    }
+   /**
+    * Sets initial block variables. Part of the blocks API
+    *
+    * @return void
+    */
+   function init() {
+      $this->title = get_string('blockname', 'block_ilp');
+   }
 
-    /**
-     * Sets up the content for the block.
-     *
-     * @return object The content object
-     */
-    function get_content() {
-        global $CFG, $USER, $COURSE, $SITE;
+   /**
+    * Sets up the content for the block.
+    *
+    * @return object The content object
+    */
+   function get_content() {
+      global $CFG, $USER, $COURSE, $SITE;
 
-        // include  db class
-        require_once($CFG->dirroot.'/blocks/ilp/db/ilp_db.php');
+      // include  db class
+      require_once($CFG->dirroot.'/blocks/ilp/db/ilp_db.php');
 
-        // include the parser class
-        require_once($CFG->dirroot.'/blocks/ilp/classes/ilp_parser.class.php');
+      // include the parser class
+      require_once($CFG->dirroot.'/blocks/ilp/classes/ilp_parser.class.php');
 
-        // include the lib file
-        require_once($CFG->dirroot.'/blocks/ilp/lib.php');
+      // include the lib file
+      require_once($CFG->dirroot.'/blocks/ilp/lib.php');
 
-        // db class manager
-        $dbc = new ilp_db();
+      // db class manager
+      $dbc = new ilp_db();
 
-        // get the course id
-        $course_id = optional_param('id', $SITE->id, PARAM_INT);
+      // get the course id
+      $course_id = optional_param('id', $SITE->id, PARAM_INT);
 
-        //this is to handle the /user/view.php page where id is reserved for the userid ...
-        //allow the current course to be course=XX
-        $current_course_id = optional_param('course', null, PARAM_INT);
-        if( !$current_course_id ){
-            $current_course_id = optional_param('id', null, PARAM_INT); //if there's no explicit course id, id might be a course id
-        }
+      //this is to handle the /user/view.php page where id is reserved for the userid ...
+      //allow the current course to be course=XX
+      $current_course_id = optional_param('course', null, PARAM_INT);
+      if( !$current_course_id ){
+         $current_course_id = optional_param('id', null, PARAM_INT); //if there's no explicit course id, id might be a course id
+      }
 
-        // get the course
-        $course = $dbc->get_course($course_id);
+      // get the course
+      $course = $dbc->get_course($course_id);
 
+      // cache the content of the block
+      if($this->content !== null) {
+         return $this->content;
+      }
 
+      //get all course that the current user is enrolled in
+      $my_courses				=	$dbc->get_user_courses($USER->id);
+      $access_viewilp			=	false;
+      $access_viewotherilp	= 	false;
 
+      if (empty($my_courses))	{
+         $c			=	new stdClass();
+         $c->id		=	$course_id;
+         $my_courses	=	array($c);
+      }
 
+      //we are going to loop through all the courses the user is enrolled in so that we can
+      //choose which display they will see
+      $found_current_course = false;
+      foreach($my_courses	as $c) {
 
-        // cache the content of the block
-        if($this->content !== null) {
-            return $this->content;
-        }
+         $sitecontext = get_context_instance(CONTEXT_SYSTEM);
+         $coursecontext = get_context_instance(CONTEXT_COURSE, $c->id);
+         $set_course_groups_link = false;
 
-    	//get all course that the current user is enrolled in
-		$my_courses				=	$dbc->get_user_courses($USER->id);
-		$access_viewilp			=	false;
-		$access_viewotherilp	= 	false;
+         //we need to get the capabilites of the current user so we can deceide what to display in the block
+         if (!empty($coursecontext) && has_capability('block/ilp:viewilp', $coursecontext,$USER->id,false) ) {
+            $access_viewilp		=	true;
+            //I have removed the var below as we dont want the my course groups link to contain
+            //the id of a  course which the user is not a teacher in
+            //$set_course_groups_link = true;
+         }
 
-		if (empty($my_courses))	{
-			$c			=	new stdClass();
-			$c->id		=	$course_id;
-			$my_courses	=	array($c);
-		}
+         if (!empty($coursecontext) && has_capability('block/ilp:viewotherilp', $coursecontext,$USER->id,false) || has_capability('block/ilp:ilpviewall', $sitecontext,$USER->id,false) || ilp_is_siteadmin($USER)) {
+            $access_viewotherilp	=	true;
+            $set_course_groups_link = true;
+         }
 
-		//we are going to loop through all the courses the user is enrolled in so that we can
-		//choose which display they will see
-        $found_current_course = false;
-		foreach($my_courses	as $c) {
+         if( $set_course_groups_link ){
+            if( !$found_current_course ){
+               $intial_course_id	=	$c->id;
+               if( $c->id == $current_course_id ){
+                  //current course is part of my_courses, so this should be the preselection for the linked page
+                  //so stop changing the value for the link
+                  $found_current_course = true;
+               }
+            }
+         }
+      }
 
-					$sitecontext = get_context_instance(CONTEXT_SYSTEM);
-        			$coursecontext = get_context_instance(CONTEXT_COURSE, $c->id);
-                    $set_course_groups_link = false;
-
-			        //we need to get the capabilites of the current user so we can deceide what to display in the block
-        			if (!empty($coursecontext) && has_capability('block/ilp:viewilp', $coursecontext,$USER->id,false) ) {
-        				$access_viewilp		=	true;
-        				//I have removed the var below as we dont want the my course groups link to contain
-        				//the id of a  course which the user is not a teacher in
-                        //$set_course_groups_link = true;
-        			}
-
-        			if (!empty($coursecontext) && has_capability('block/ilp:viewotherilp', $coursecontext,$USER->id,false) || has_capability('block/ilp:ilpviewall', $sitecontext,$USER->id,false) || ilp_is_siteadmin($USER)) {
-        				$access_viewotherilp	=	true;
-                        $set_course_groups_link = true;
-        			}
-
-                    if( $set_course_groups_link ){
-	                    if( !$found_current_course ){
-	        				$intial_course_id	=	$c->id;
-	                        if( $c->id == $current_course_id ){
-	                            //current course is part of my_courses, so this should be the preselection for the linked page
-	                            //so stop changing the value for the link
-	                            $found_current_course = true;
-	                        }
-	                    }
-                    }
-		}
-
-		//
-		$usertutees	=	$dbc->get_user_tutees($USER->id);
+      //
+      $usertutees	=	$dbc->get_user_tutees($USER->id);
 
 
-		$this->content = new stdClass;
-        $this->content->footer = '';
+      $this->content = new stdClass;
+      $this->content->footer = '';
 
-        //check if the user has the viewotherilp capability
-        if (!empty($access_viewotherilp) || !empty($usertutees)) {
+      //check if the user has the viewotherilp capability
+      if (!empty($access_viewotherilp) || !empty($usertutees)) {
 
-        	if (!empty($access_viewotherilp)) {
-			 	$label = get_string('mycoursegroups', 'block_ilp');
-	         	$url  = "{$CFG->wwwroot}/blocks/ilp/actions/view_studentlist.php?tutor=0&course_id={$intial_course_id}";
-	         	$this->content->items[] = "<a href='{$url}'>{$label}</a>";
-	         	$this->content->icons[] = "";
-    		}
+         if (!empty($access_viewotherilp)) {
+            $label = get_string('mycoursegroups', 'block_ilp');
+            $url  = "{$CFG->wwwroot}/blocks/ilp/actions/view_studentlist.php?tutor=0&course_id={$intial_course_id}";
+            $this->content->items[] = "<a href='{$url}'>{$label}</a>";
+            $this->content->icons[] = "";
+         }
 
-        	if (!empty($usertutees)) {
-			 	$label = get_string('mytutees', 'block_ilp');
-	         	$url  = "{$CFG->wwwroot}/blocks/ilp/actions/view_studentlist.php?tutor=1&course_id=0";
-	         	$this->content->items[] = "<a href='{$url}'>{$label}</a>";
-	         	$this->content->icons[] = "";
-    		}
+         if (!empty($usertutees)) {
+            $label = get_string('mytutees', 'block_ilp');
+            $url  = "{$CFG->wwwroot}/blocks/ilp/actions/view_studentlist.php?tutor=1&course_id=0";
+            $this->content->items[] = "<a href='{$url}'>{$label}</a>";
+            $this->content->icons[] = "";
+         }
 
 
-        } else {
+      } else {
 
 
 
-        	//additional check to stop users from being able to access the ilp in course context
-        	//from the front page
-        	$courseurl	=	(!empty($course_id) && $course_id != 1) ? "&course_id={$course_id}" : '';
+         //additional check to stop users from being able to access the ilp in course context
+         //from the front page
+         $courseurl	=	(!empty($course_id) && $course_id != 1) ? "&course_id={$course_id}" : '';
 
-	       	$this->content->text	= "";
+         $this->content->text	= "";
 
-			$label = get_string('mypersonallearningplan', 'block_ilp');
-	        $url  = "{$CFG->wwwroot}/blocks/ilp/actions/view_main.php?user_id={$USER->id}$courseurl";
-	        $this->content->items[] = "<p><a href='{$url}'>{$label}</a><p/>";
-	        $this->content->icons[] = "";
+         $label = get_string('mypersonallearningplan', 'block_ilp');
+         $url  = "{$CFG->wwwroot}/blocks/ilp/actions/view_main.php?user_id={$USER->id}$courseurl";
+         $this->content->items[] = "<p><a href='{$url}'>{$label}</a><p/>";
+         $this->content->icons[] = "";
 
 
 
 
 
 
-        }
+      }
 
 
 
 
-        return $this->content;
-    }
+      return $this->content;
+   }
 
 
 
-    /**
-     * Allow the user to set sitewide configuration options for the block.
-     *
-     * @return bool true
-     */
-    function has_config() {
-        return true;
-    }
+   /**
+    * Allow the user to set sitewide configuration options for the block.
+    *
+    * @return bool true
+    */
+   function has_config() {
+      return true;
+   }
 
 
-    /**
-     * Allow the user to set specific configuration options for the instance of
-     * the block attached to a course.
-     *
-     * @return bool true
-     */
-    function instance_allow_config() {
+   /**
+    * Allow the user to set specific configuration options for the instance of
+    * the block attached to a course.
+    *
+    * @return bool true
+    */
+   function instance_allow_config() {
 
-        return false;
-    }
-
-
+      return false;
+   }
 
 
-    /**
-     * Only allow this block to be mounted to a course or the home page.
-     *
-     * @return array
-     */
-    function applicable_formats() {
-        return array(
-            'site-index'  => true,
-            'course-view' => true,
-        );
-    }
 
-    /**
-     * Prevent the user from having more than one instance of the block on each
-     * course.
-     *
-     * @return bool false
-     */
-    function instance_allow_multiple() {
-        return false;
-    }
 
-    /*
-     * Functions that we want to run directly after the block has been installed
-     *
-     */
-	function after_install() {
+   /**
+    * Only allow this block to be mounted to a course or the home page.
+    *
+    * @return array
+    */
+   function applicable_formats() {
+      return array(
+         'site-index'  => true,
+         'course-view' => true,
+         );
+   }
 
-		global $CFG;
+   /**
+    * Prevent the user from having more than one instance of the block on each
+    * course.
+    *
+    * @return bool false
+    */
+   function instance_allow_multiple() {
+      return false;
+   }
 
-		//call the install.php script (used for moodle 2) that has the operations that need to be carried
-		//out after installation
-		require_once($CFG->dirroot.'/blocks/ilp/db/install.php');
+   /*
+    * Functions that we want to run directly after the block has been installed
+    *
+    */
+   function after_install() {
 
-		//call the block_ilp_install function used by moodle 2.0
-		xmldb_block_ilp_install();
+      global $CFG;
 
-	}
+      //call the install.php script (used for moodle 2) that has the operations that need to be carried
+      //out after installation
+      require_once($CFG->dirroot.'/blocks/ilp/db/install.php');
 
-	function cron() {
-		global $CFG;
-		require_once($CFG->dirroot."/blocks/ilp/classes/ilp_cron.class.php");
-		$cron	=	 new ilp_cron();
-		$cron->run();
-	}
+      //call the block_ilp_install function used by moodle 2.0
+      xmldb_block_ilp_install();
+
+   }
+
+   function cron() {
+      global $CFG;
+      require_once($CFG->dirroot."/blocks/ilp/classes/ilp_cron.class.php");
+      $cron	=	 new ilp_cron();
+      $cron->run();
+   }
 
 }
 ?>
