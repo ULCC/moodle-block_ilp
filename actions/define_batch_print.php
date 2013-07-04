ee71fb3b2eb56228c12e8ce7188e7808de010104
Thomas Worthington <thomas.worthington@london.ac.uk>
2013-07-04 13:40:20 +0100

    Rearranged form include vis-a-vis parameters

--- actions/define_batch_print.php
+++ actions/define_batch_print.php
@@ -2,7 +2,7 @@
 /**
  * Allows the user to print a list of students' reports
  *
- * @copyright &copy; 2011 University of London Computer Centre
+ * @copyright &copy; 2013 University of London Computer Centre
  * @author http://www.ulcc.ac.uk, http://moodle.ulcc.ac.uk
  * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
  * @package ILP
@@ -12,28 +12,6 @@
 require_once('../configpath.php');
 
 require_once("$CFG->dirroot/blocks/ilp/classes/forms/batch_print_setup_mform.php");
-
-$mform=new batch_print_setup_mform($PAGE->url,array('course_id'=>optional_param('course_id',0,PARAM_INT),
-                                                    'tutor'=>optional_param('tutor',0,PARAM_INT),
-                                                    'group_id'=>optional_param('group_id',0,PARAM_INT),
-                                                    'status_id'=>optional_param('status_id',0,PARAM_INT)));
-
-if($mform->is_cancelled())
-{
-   redirect("$CFG->wwwroot");
-}
-elseif($data=$mform->get_data())
-{
-//Only possible if url has been twiddled, so slap them back to the front page
-   if(empty($data->course_id))
-   {
-      redirect($CFG->wwwroot);
-   }
-
-   print_object($data);
-   include("$CFG->dirroor/blocks/ilp/views/print_preview.php");
-   exit;
-}
 
 //include any necessary files
 
@@ -54,6 +32,30 @@ $group_id = $PARSER->optional_param('group_id', 0, PARAM_INT);
 
 // instantiate the db
 $dbc = new ilp_db();
+
+$baseurl = new moodle_url($CFG->wwwroot."/blocks/ilp/actions/define_batch_print.php",PARSER->get_params());
+
+$mform=new batch_print_setup_mform($baseurl->out(),array('course_id'=>optional_param('course_id',0,PARAM_INT),
+                                                         'tutor'=>optional_param('tutor',0,PARAM_INT),
+                                                         'group_id'=>optional_param('group_id',0,PARAM_INT),
+                                                         'status_id'=>optional_param('status_id',0,PARAM_INT)));
+
+if($mform->is_cancelled())
+{
+   redirect("$CFG->wwwroot");
+}
+elseif($data=$mform->get_data())
+{
+//Only possible if url has been twiddled, so slap them back to the front page
+   if(empty($data->course_id))
+   {
+      redirect($CFG->wwwroot);
+   }
+
+   print_object($data);
+   include("$CFG->dirroor/blocks/ilp/views/print_preview.php");
+   exit;
+}
 
 //check if the any of the users roles in the
 //current context has the create report capability for this report
@@ -82,13 +84,12 @@ $title = get_string('print','block_ilp');
 $PAGE->navbar->add($title,null,'title');
 
 // setup the page title and heading
-$baseurl = $CFG->wwwroot."/blocks/ilp/actions/define_batch_print.php";
 
 $SITE = $dbc->get_course_by_id(SITEID);
 $PAGE->set_title($SITE->fullname." : ".get_string('blockname','block_ilp'));
 $PAGE->set_heading($SITE->fullname);
 $PAGE->set_pagetype('ilp-reportlist');
 $PAGE->set_pagelayout('ilp');
-$PAGE->set_url($baseurl,$PARSER->get_params());
+$PAGE->set_url($baseurl);
 
 require_once($CFG->dirroot.'/blocks/ilp/views/batch_print.html');
