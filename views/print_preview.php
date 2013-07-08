85752d807cb00bbbb312bc588b0ee5781cda0aca
Thomas Worthington <thomas.worthington@london.ac.uk>
2013-07-08 11:49:57 +0100

    Switch print preview to new class

--- views/print_preview.php
+++ views/print_preview.php
@@ -81,7 +81,7 @@ require_once("$CFG->dirroot/blocks/ilp/plugins/dashboard/ilp_dashboard_student_i
 
 foreach($dbc->get_studentlist_details(array_keys($students),$status_id,'','lastname asc') as $student)
 {
-   $info=new ilp_dashboard_student_info_plugin($student->id);
+   $info=new ilp_dashboard_student_info_print_plugin($student->id);
 
    print $info->display();
 
