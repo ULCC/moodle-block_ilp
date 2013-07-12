4a24d1f0ee9e89bd4fda33b3837c55a63514f513
Thomas Worthington <thomas.worthington@london.ac.uk>
2013-07-12 13:57:02 +0100

    Cache update

--- version.php
+++ version.php
@@ -11,7 +11,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2013071201;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2013071203;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2012120300;        // Requires this Moodle version
 
 $plugin->component = 'block_ilp';      // Full name of the plugin (used for diagnostics)
