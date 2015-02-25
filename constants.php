91f19aa9ecd362889a34e8dec938c78515827c6e
aferenz <a.ferenz@ulcc.ac.uk>
2015-02-25 15:26:09 +0000

    changed pagelayout to 'standard' #88224712

--- constants.php
+++ constants.php
@@ -140,14 +140,4 @@ define('ILP_DATEFIELD_REVIEWDATE', 2);
 
 define('ILP_CORE_TAB_PLUGINS', 'ilp_dashboard_archive_tab|ilp_dashboard_entries_tab|ilp_dashboard_reports_tab|ilp_dashboard_vault_tab');
 
-try { // ILP does some dodgy lang string includes during install so we need try/catch in case config tables don't exist yet
-    $pagelayout = get_config('block_ilp', 'pagelayout');
-} catch (dml_exception $e) {
-    $pagelayout = '';
-}
-
-if (!empty($pagelayout)) {
-    define('ILP_PAGELAYOUT', $pagelayout);
-} else {
-    define('ILP_PAGELAYOUT', 'standard');
-}
+define('ILP_PAGELAYOUT', 'standard');
